9547c395467e78f2aa0b007b97d78b707d75968a
bmoore10 <bmoore10@illinois.edu>
2019-03-06 01:12:03 -0600

    Various changes, all described in the new changelog

--- Main/Bot.py
+++ Main/Bot.py
@@ -15,6 +15,9 @@ from NG_Scraper import main as Scrape
 # Start fresh with scraper data. We want to stay up to date
 scraper_data = None
 
+# Flag value to stop users from adding artists during database update
+canAdd = True
+
 # Set up the client, prefix is !
 bot = commands.Bot(command_prefix='!')
 
@@ -57,11 +60,11 @@ async def pic(ctx, *, message: str):
     if message in scraper_data:
         images = scraper_data[message]["deep_links"]
         if len(images) < 1:
-            await ctx.send("No images stored for " + message + " :(")
+            await ctx.send("`No images stored for " + message + " :(`")
         else:
             await ctx.send(random.choice(images))
     else:
-        await ctx.send(message + " is not in the database :(")
+        await ctx.send("`" + message + " is not in the database :(`")
 
 
 # Adds an artist to the scraper db to be scraped next time the database is updated.
@@ -69,14 +72,18 @@ async def pic(ctx, *, message: str):
 @bot.command()
 @commands.check(check_if_appropriate_channel)
 async def add_artist(ctx, *, message: str):
-    await refresh_scraper_data()
+    global canAdd
+    if canAdd:
+        await refresh_scraper_data()
 
-    if message in scraper_data['artist_urls']:
-        await ctx.send(message + " is already in the database :)")
+        if message in scraper_data['artist_urls']:
+            await ctx.send("`" + message + " is already in the database :)`")
+        else:
+            scraper_data['artist_urls'].append(message)
+            await store_scraper_data()
+            await ctx.send("`Added <" + message + "> to artist urls`")
     else:
-        scraper_data['artist_urls'].append(message)
-        await store_scraper_data()
-        await ctx.send("Added <" + message + "> to artist urls")
+        await ctx.send("`Database update in progress - no artists may be added until this process finishes.`")
 
 
 # Lists all artists in the scraper db whose pics can be requested
@@ -85,38 +92,46 @@ async def add_artist(ctx, *, message: str):
 async def artists(ctx):
     await refresh_scraper_data()
 
-    await ctx.send('`Artist list:`')
+    response = ("```\nArtist list:\n")
     for artist in scraper_data.keys():
         if artist != 'artist_urls':
-            await ctx.send(artist + "(" + str(len(scraper_data[artist]["deep_links"])) + ")")
+            response += (artist + " (" + str(len(scraper_data[artist]["deep_links"])) + " images)\n")
+    await ctx.send(response + "\n```")
 
 
 # Updates the bot's database by running the scraper
 @bot.command()
 @commands.check(check_if_appropriate_channel)
 async def update_database(ctx):
+    global canAdd
     await refresh_scraper_data()
-    await ctx.send("`Updating Database - Please don't make any requests until I'm finished.`")
-    response = Scrape()
+    await ctx.send("`Updating Database - No artists may be added until I am finished`")
+    canAdd = False
+    response = await bot.loop.run_in_executor(None, Scrape)
 
-    await ctx.send("`Finished updating database.`\n Summary:")
+    update_report = "```\nFinished updating database.\nUpdate summary:\n"
     for output in response:
-        await ctx.send(output)
+        update_report += (output + "\n")
+
+    canAdd = True
+    await ctx.send(update_report + "\n```")
 
 
 # Lists commands as well as how they should be used
 @bot.command()
 @commands.check(check_if_appropriate_channel)
 async def commands(ctx):
-    response = [
-                 "`!commands` - Gets you this list again!",
-                 "`!add_artist [url]` - Adds an artist to the database. url should be formatted <https://artist.newgrounds.com/art>",
-                 "`!pic [artist_name]` - Retrieves a random image from the database by the artist passed in.",
-                 "`!artists` - Lists which artists images can be requested based upon who's in the database and the amount of images we have stored.",
-                 "`!update_database` - Runs the scraper to update images stored in the database"
-               ]
-    for command in response:
-        await ctx.send(command)
+    response = (
+                 "```\n"
+                 "!commands - Gets you this list again!\n"
+                 "!add_artist [url] - Adds an artist to the database. url should be formatted <https://artist.newgrounds.com/art>\n"
+                 "!pic [artist_name] - Retrieves a random image from the database by the artist passed in.\n"
+                 "!artists - Lists which artists images can be requested based upon who's in the database and the amount of images we have stored.\n"
+                 "!update_database - Runs the scraper to update images stored in the database\n"
+                 "```"
+               )
+
+    await ctx.send(response)
 
 # Start 'er up
 bot.run(configData['token'])
